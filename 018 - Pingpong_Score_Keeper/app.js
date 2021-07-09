0c425d2b1ffa7a2d2a7d3ece8582e7e78752399b
Xinyi Wu <xw2357@columbia.edu>
2021-07-08 22:02:50 -0700

    score keeper basic logic

--- 018 - Pingpong_Score_Keeper/app.js
+++ 018 - Pingpong_Score_Keeper/app.js
@@ -4,6 +4,10 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#winningScoreSelect');
+
+
 
 let p1Score = 0;
 let p2Score = 0;
@@ -15,6 +19,8 @@ p1Button.addEventListener('click', function (e) {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
     }
@@ -25,7 +31,27 @@ p2Button.addEventListener('click', function (e) {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('loser');
+            p2Display.classList.add('winner');
         }
         p2Display.textContent = p2Score;
     }
 });
+
+winningScoreSelect.addEventListener('change', function (e) {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+
+}
