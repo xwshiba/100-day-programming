9d74407dd2aee72e7d7bc695e2dd4fbdfdcc6c93
Xinyi Wu <xw2357@columbia.edu>
2021-07-09 10:33:22 -0700

    refactoring the js

--- 018 - Pingpong_Score_Keeper/app.js
+++ 018 - Pingpong_Score_Keeper/app.js
@@ -1,45 +1,41 @@
-// alert("jfiewjao")
+const player1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
 
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
+const player2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#winningScoreSelect');
 
-
-
-let p1Score = 0;
-let p2Score = 0;
 let isGameOver = false;
 
-p1Button.addEventListener('click', function (e) {
+function updateScores(player, opponent) {
     if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winningScore) {
+        player.score += 1;
+        if (player.score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
-            p1Button.disabled = true; // part of the Bulma event
-            p2Button.disabled = true; // part of the Bulma event
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true; // part of the Bulma event
+            opponent.button.disabled = true; // part of the Bulma event
         }
-        p1Display.textContent = p1Score;
+        player.display.textContent = player.score;
     }
-});
+}
 
-p2Button.addEventListener('click', function (e) {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winningScore) {
-            isGameOver = true;
-            p1Display.classList.add('has-text-danger');
-            p2Display.classList.add('has-text-success');
-            p1Button.disabled = true; // part of the Bulma event
-            p2Button.disabled = true; // part of the Bulma event
-        }
-        p2Display.textContent = p2Score;
-    }
-});
+player1.button.addEventListener('click', function (e) {
+    updateScores(player1, player2);
+})
+
+player2.button.addEventListener('click', function (e) {
+    updateScores(player1, player2);
+})
 
 winningScoreSelect.addEventListener('change', function (e) {
     winningScore = parseInt(this.value);
@@ -50,13 +46,10 @@ resetButton.addEventListener('click', reset);
 
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
-    p2Display.classList.remove('has-text-success', 'has-text-danger');
-    p1Button.disabled = false; // part of the Bulma event
-    p2Button.disabled = false; // part of the Bulma event
-
+    for (let player of [player1, player2]) {
+        player.score = 0;
+        player.display.textContent = 0;
+        player.display.classList.remove('has-text-success', 'has-text-danger');
+        player.button.disabled = false;
+    }
 }
